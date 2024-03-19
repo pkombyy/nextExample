3543cae37f87e4a04dcc678ef522d0c11e839d56
pkombyy <mlgenergy36@gmail.com>
2024-03-19 11:49:42 +0500

    feat: 9 chapter

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,13 +11,13 @@ import clsx from "clsx";
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Домой', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
+    name: 'Накладные',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Заказчики', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
